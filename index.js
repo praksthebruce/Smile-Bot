8c9b5d28531e5945041cf97f1d0d5008e0ee4e55
praksthebruce <praksthebruce@gmail.com>
2026-08-23 11:08:57 +0530

    Update Smile Bot

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const path = require("path");
 const axios = require("axios");
 
-require("dotenv").config({ path: path.join(__dirname, "Smilebot") });
+require("dotenv").config({ path: path.join(__dirname, ".env") });
 
 
 const { App } = require("@slack/bolt");
 
 if (!process.env.SLACK_BOT_TOKEN || !process.env.SLACK_APP_TOKEN) {
   throw new Error(
-    "Missing Slack credentials. Set SLACK_BOT_TOKEN and SLACK_APP_TOKEN in Smilebot."
+    "Missing Slack credentials. Set SLACK_BOT_TOKEN and SLACK_APP_TOKEN in .env."
   );
 }
 
